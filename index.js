8a6d3fc56d0bdb009c9d317d44b3608c3800053d
juancjara <juanc.jara@pucp.pe>
2018-09-25 19:25:19 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const puppeteer = require("puppeteer");
+const http = require("http");
+const { parse } = require("url");
+
+const getImageBuffer = async (url, selector) => {
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  await page.waitForSelector(selector);
+  const element = await page.$(selector);
+  const buffer = await element.screenshot();
+
+  await browser.close();
+  return buffer;
+};
+
+module.exports = async (req, res) => {
+  try {
+    const { url, selector } = parse(req.url, true).query;
+    console.log('url', url, 'selector', selector)
+    const buffer = await getImageBuffer(url, selector);
+    res.end(buffer, "binary");
+  } catch (error) {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    return { error };
+  }
+};
